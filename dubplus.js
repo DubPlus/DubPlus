6fba87a6a753cb486792369b29be90be882ebd81
FranciscoG <frankjg@gmail.com>
2017-03-07 16:13:12 -0500

    deleting generated extension folders

--- dubplus.js
+++ dubplus.js
@@ -3077,7 +3077,7 @@ module.exports = {
   loadExternal: loadExternal
 };
 
-}).call(this,'1488919654731')
+}).call(this,'1488921116263')
 },{"../lib/settings.js":8}],40:[function(require,module,exports){
 'use strict';
 
