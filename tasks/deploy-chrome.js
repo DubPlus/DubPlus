2d7434f6c4d7ef1c1f5bcb7f24b5538d3cb2bc9f
franciscog <frankjg@gmail.com>
2017-03-08 08:16:24 -0500

    new icons for ext

--- tasks/deploy-chrome.js
+++ tasks/deploy-chrome.js
@@ -1,18 +1,29 @@
 var request = require('request');
 var fs = require('fs');
 
+// you need to pass all 4 of these variables via ENV vars or in a private.json file in the
+// root of the repo or this whole.  .gitignore is setup to ignore private.json
+try {
+  var private = require('../private.json');
+  const CHROME_EXT_ITEM_ID =  process.env.CHROME_EXT_ITEM_ID || private.CHROME_EXT_ITEM_ID;
+  const CLIENT_ID = process.env.CLIENT_ID || private.CLIENT_ID;
+  const CLIENT_SECRET = process.env.CLIENT_SECRET || private.CLIENT_SECRET;
+  const CODE = process.env.CODE || private.CODE;
+} catch(ex) {
+  console.log("missing environment variables");
+  console.error(ex);
+  process.exit(1);
+}
+
 /*
   Note: 
   Currently, there is no API for setting an item’s metadata, such as description. This has to be done manually in the Chrome Web Store Developer Dashboard.
  */
 
-function getChromeOauthToken(CLIENT_ID, CLIENT_SECRET, CODE, cb){
+function getChromeOauthToken(cb){
   var options = {
     url: 'https://accounts.google.com/o/oauth2/token',
-    headers: {
-      'x-goog-api-version': '2'
-    },
-    form: {
+    formData: {
       client_id: CLIENT_ID,
       client_secret: CLIENT_SECRET,
       code: CODE,
@@ -33,30 +44,35 @@ function getChromeOauthToken(CLIENT_ID, CLIENT_SECRET, CODE, cb){
 
 }
 
+// Uploading a package to update an existing store item
 function updateExtension(TOKEN, cb) {
   
   var options = {
-    url: 'https://developer.chrome.com/webstore/webstore_api/items/update',
+    url: `https://www.googleapis.com/upload/chromewebstore/v1.1/items/${CHROME_EXT_ITEM_ID}`,
     headers: {
-      'x-goog-api-version': '2'
-    },
-    formData : {
-        Authorization: 'Bearer ${TOKEN}',
-        my_file: fs.createReadStream(__dirname + '/unicycle.jpg'),
+      'x-goog-api-version': '2',
+      'Authorization': 'Bearer ${TOKEN}'
     }
   };
 
-  request.put(options, 
-    function optionalCallback(err, httpResponse, body) {
-      if (err) {
-        console.error('upload failed:', err);
-        process.exit(1);
+  fs.createReadStream(process.cwd() + '/extensions/Chrome.zip')
+    .pipe(request.put(options, 
+      function(err, httpResponse, body) {
+        if (err) {
+          console.error('upload failed:', err);
+          process.exit(1);
+        }
+        cb(body);
       }
-      cb(body);
-    }
-  );
+    ));
 }
 
 module.exports = function(){
 
-};
+  // step 1 zip the folder
+  // step 2 get access token
+  // step 3 send update to the api
+  getChromeOauthToken(function(result){
+    console.log(result);
+  });
+}; 
