6437b58d41ce8310ef9796e31c31d0da45c03732
franciscog <frankjg@gmail.com>
2017-03-08 08:43:49 -0500

    added a changelog, working on extension deploy tasks still

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+This project adheres to [Semantic Versioning](http://semver.org/).
+
+## [0.1.1] - 2017-03-07
+### Changed
+- updated extension icons
+
+## [0.1.0] - 2017-03-06
+### Added
+- Initial build of script.  Ready for testing and deploying. 
--- tasks/deploy-ext.js
+++ tasks/deploy-ext.js
@@ -1,4 +1,35 @@
+var exec = require('child_process').exec;
+var chrome_ext = require('./deploy-chrome.js');
+
+/**
+ * zips up folders for deployment to chrome/FF extentions stores
+ * @param  {String} dir directory to zip up
+ * @return {undefined}
+ */
+function doZip(dir){
+  console.log(process.cwd() + `/extensions/${dir}`);
+  //zip [options] zipfile.zip files-to-zip
+  exec(`zip -vr ${dir} ${dir} -x "*.DS_Store"`,
+    {cwd: process.cwd() + `/extensions`},
+    function(error, stdout, stderr){
+      //console.log('stdout: ' + stdout);
+      if (stderr !== "") {
+        console.log('stderr: ' + stderr);
+      }
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      }
+  });
+}
+
 module.exports = function(){
-  console.log("Eventually this will be a full extension deploy script");
+  
+  // step 1, zip up both folders:
+  ['Chrome','Firefox'].forEach(function(dir){
+    doZip(dir);
+  });
+
+  // step 2
+  
 };
 
