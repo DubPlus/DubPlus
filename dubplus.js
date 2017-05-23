6c5c1216a22916d0a86c245d3df836d400016f42
franciscog <frankjg@gmail.com>
2017-05-22 22:02:03 -0400

    forgot to bundle

--- dubplus.js
+++ dubplus.js
@@ -872,7 +872,7 @@ afk_module.moduleName = "AFK Auto-respond";
 afk_module.description = "Toggle Away from Keyboard and customize AFK message.";
 afk_module.category = "General";
 
-var afk_chat_respond = function afk_chat_respond(e) {
+afk_module.afk_chat_respond = function (e) {
   if (!this.optionState) {
     return; // do nothing until it's back to true
   }
@@ -898,11 +898,11 @@ var afk_chat_respond = function afk_chat_respond(e) {
 };
 
 afk_module.turnOn = function () {
-  Dubtrack.Events.bind("realtime:chat-message", afk_chat_respond.bind(this));
+  Dubtrack.Events.bind("realtime:chat-message", this.afk_chat_respond.bind(this));
 };
 
 afk_module.turnOff = function () {
-  Dubtrack.Events.unbind("realtime:chat-message", afk_chat_respond);
+  Dubtrack.Events.unbind("realtime:chat-message", this.afk_chat_respond);
 };
 
 var saveAFKmessage = function saveAFKmessage() {
@@ -3138,7 +3138,7 @@ module.exports = {
   loadExternal: loadExternal
 };
 
-}).call(this,'1495503823359')
+}).call(this,'1495504721028')
 },{"../lib/settings.js":8}],41:[function(require,module,exports){
 'use strict';
 
