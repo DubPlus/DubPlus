a54da4e47f957f2b6f629de85d5299f7f025fde3
franciscog <frankjg@gmail.com>
2019-01-31 22:55:43 -0500

    improve emoji picker with lazy loading

--- src/js2/components/emoji-picker.js
+++ src/js2/components/emoji-picker.js
@@ -1,11 +1,15 @@
 import { h, render, Component } from "preact";
 import { emoji } from "../utils/emotes/prepEmoji";
+import emojis from '../utils/emotes/emoji';
 import Portal from "preact-portal/src/preact-portal";
 
 class EmojiPicker extends Component {
   state = {
-    show: false
+    show: false,
+    emojiLoad: 70
   };
+
+  isLoading = false
 
   fillChat(val) {
     document.getElementById("chat-txt-message").value += " " + val;
@@ -15,30 +19,57 @@ class EmojiPicker extends Component {
     this.setState(prevState => {
       return { show: !prevState.show };
     });
-  }
+  };
 
   componentDidMount() {
-    document.addEventListener('keyup', (e) => {
+    document.addEventListener("keyup", e => {
       const key = "which" in e ? e.which : e.keyCode;
       if (this.state.show && key === 27) {
-        this.setState({show: false})
+        this.setState({ show: false });
       }
     });
   }
 
-  render(props,{show}) {
-    let list = emojify.emojiNames.map(e => {
+  onScroll = (e) => {
+    const el = e.target;
+    if (el.scrollTop + el.clientHeight > (el.scrollHeight - 25) && !this.isLoading ) {
+      this.isLoading = true;
+      this.setState((prevState)=>{
+        let next = prevState.emojiLoad + 70;
+        if (next >= emojis.length) { 
+          next = emojis.length;
+          document.querySelector('.dp-emoji-picker').removeEventListener('scroll', this.onScroll);
+        }
+        return { emojiLoad: next }
+      }, ()=>{
+        this.isLoading = false;
+      })
+    }
+  }
+
+  componentDidMount() {
+    document.querySelector('.dp-emoji-picker').addEventListener('scroll', this.onScroll);
+  }
+
+  render(props, { show, emojiLoad }) {
+    let list = emojis.slice(0, emojiLoad).map(e => {
+      let name = e.replace(/^:|:$/g, '');
       return (
-        <span onClick={() => this.fillChat(`:${e}:`)}>
-          <img src={emoji.template(e)} />
+        <span key={`emoji-${name}`} onClick={() => this.fillChat(e)}>
+          <img src={emoji.template(name)} />
         </span>
       );
     });
 
     return (
-      <span className="dp-emoji-picker-icon fa fa-smile-o" onClick={this.toggle}>
+      <span
+        className="dp-emoji-picker-icon fa fa-smile-o"
+        onClick={this.toggle}
+      >
         <Portal into=".pusher-chat-widget-input">
-          <div className={`dp-emoji-picker ${show?'show':''}`}>{list}</div>
+          <div className={`dp-emoji-picker ${show ? "show" : ""}`}>
+            {list}
+          </div>
         </Portal>
       </span>
     );
