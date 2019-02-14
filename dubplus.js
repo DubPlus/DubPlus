2e049e5a9898a722d7f3dc50fec67a3796420aba
franciscog <frankjg@gmail.com>
2019-02-14 15:55:38 -0500

    merging in git hooks branch

--- dubplus.js
+++ dubplus.js
@@ -11286,7 +11286,7 @@ var DubPlus = (function () {
     function UserSettings() {
       _classCallCheck(this, UserSettings);
 
-      _defineProperty(this, "srcRoot", "https://cdn.jsdelivr.net/gh/FranciscoG/DubPlus@add-githooks");
+      _defineProperty(this, "srcRoot", "https://cdn.jsdelivr.net/gh/DubPlus/DubPlus@beta");
 
       var _savedSettings = localStorage.getItem('dubplusUserSettings');
 
@@ -14898,7 +14898,7 @@ var DubPlus = (function () {
       return;
     }
 
-    var link = makeLink(className, userSettings.srcRoot + cssFile + "?" + 1550177632409);
+    var link = makeLink(className, userSettings.srcRoot + cssFile + "?" + 1550177743963);
     document.head.appendChild(link);
   }
   /**
