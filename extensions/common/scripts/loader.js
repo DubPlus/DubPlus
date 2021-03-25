f156785c52a00666aed519fe6f27a72f2531e120
Cory Shaw <cory.shaw.dev@gmail.com>
2021-03-25 10:40:20 -0400

    update(extensions): Fix unneccessary Chrome permissions, fix Firefox extension load

--- extensions/common/scripts/loader.js
+++ extensions/common/scripts/loader.js
@@ -1,1 +1,6 @@
-$.getScript('https://cdn.jsdelivr.net/gh/DubPlus/DubPlus/dubplus.js');
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.async = true;
+script.src = 'https://cdn.jsdelivr.net/gh/DubPlus/DubPlus/dubplus.js';
+
+document.getElementsByTagName('head')[0].appendChild(script);
