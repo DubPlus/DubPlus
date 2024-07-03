5214149a89ea7b4f16e6289da0b4cf9fa7186180
franciscog <557684+FranciscoG@users.noreply.github.com>
2024-07-03 15:23:05 -0400

    think I finally fixed the autocompelte multline issue

--- src/js/modules/autocomplete.js
+++ src/js/modules/autocomplete.js
@@ -2,9 +2,9 @@
  * Autocomplete Emojis/Emotes
  */
 /*global _, Dubtrack, emojify*/
-var settings = require("../lib/settings.js");
-var prepEmjoji = require("../emojiUtils/prepEmoji.js");
-import { PreviewListManager, makeList } from "../emojiUtils/previewList.js";
+var settings = require('../lib/settings.js');
+var prepEmjoji = require('../emojiUtils/prepEmoji.js');
+import { PreviewListManager, makeList } from '../emojiUtils/previewList.js';
 
 // because I have a lot of logging on each keypress I made this
 var debugAC = false;
@@ -15,11 +15,11 @@ function log() {
 }
 
 var myModule = {};
-myModule.id = "dubplus-autocomplete";
-myModule.moduleName = "Autocomplete Emoji";
+myModule.id = 'dubplus-autocomplete';
+myModule.moduleName = 'Autocomplete Emoji';
 myModule.description =
-  "Toggle autocompleting emojis and emotes.  Shows a preview box in the chat";
-myModule.category = "General";
+  'Toggle autocompleting emojis and emotes.  Shows a preview box in the chat';
+myModule.category = 'General';
 
 const KEYS = {
   up: 38,
@@ -37,7 +37,7 @@ const KEYS = {
 };
 
 const keyCharMin = 3; // when to start showing previews
-const inputRegex = new RegExp("(:)([&!()\\+\\-_a-z0-9]+)($|\\s)", "ig");
+const inputRegex = new RegExp('(:)([&!()\\+\\-_a-z0-9]+)($|\\s)', 'ig');
 
 /**************************************************************************
  * A bunch of utility helpers for the emoji preview
@@ -46,7 +46,7 @@ var emojiUtils = {
   createPreviewObj: function (type, id, name) {
     return {
       src: prepEmjoji[type].template(id),
-      text: ":" + name + ":",
+      text: ':' + name + ':',
       alt: name,
       cn: type,
     };
@@ -59,30 +59,30 @@ var emojiUtils = {
 
     emojiArray.forEach(function (val) {
       _key = val.toLowerCase();
-      if (typeof prepEmjoji.twitch.emotes[_key] !== "undefined") {
+      if (typeof prepEmjoji.twitch.emotes[_key] !== 'undefined') {
         listArray.push(
-          self.createPreviewObj("twitch", prepEmjoji.twitch.emotes[_key], val)
+          self.createPreviewObj('twitch', prepEmjoji.twitch.emotes[_key], val)
         );
       }
-      if (typeof prepEmjoji.bttv.emotes[_key] !== "undefined") {
+      if (typeof prepEmjoji.bttv.emotes[_key] !== 'undefined') {
         listArray.push(
-          self.createPreviewObj("bttv", prepEmjoji.bttv.emotes[_key], val)
+          self.createPreviewObj('bttv', prepEmjoji.bttv.emotes[_key], val)
         );
       }
-      if (typeof prepEmjoji.tasty.emotes[_key] !== "undefined") {
-        listArray.push(self.createPreviewObj("tasty", _key, val));
+      if (typeof prepEmjoji.tasty.emotes[_key] !== 'undefined') {
+        listArray.push(self.createPreviewObj('tasty', _key, val));
       }
-      if (typeof prepEmjoji.frankerFacez.emotes[_key] !== "undefined") {
+      if (typeof prepEmjoji.frankerFacez.emotes[_key] !== 'undefined') {
         listArray.push(
           self.createPreviewObj(
-            "frankerFacez",
+            'frankerFacez',
             prepEmjoji.frankerFacez.emotes[_key],
             val
           )
         );
       }
       if (emojify.emojiNames.indexOf(_key) >= 0) {
-        listArray.push(self.createPreviewObj("emoji", val, val));
+        listArray.push(self.createPreviewObj('emoji', val, val));
       }
     });
 
@@ -90,10 +90,10 @@ var emojiUtils = {
   },
 
   filterEmoji: function (str) {
-    var finalStr = str.replace(/([+()])/, "\\$1");
-    var re = new RegExp("^" + finalStr, "i");
+    var finalStr = str.replace(/([+()])/, '\\$1');
+    var re = new RegExp('^' + finalStr, 'i');
     var arrayToUse = emojify.emojiNames || [];
-    if (settings.options["dubplus-emotes"]) {
+    if (settings.options['dubplus-emotes']) {
       arrayToUse = prepEmjoji.emojiEmotes || []; // merged array
     }
 
@@ -113,13 +113,13 @@ var shouldClearPreview = function (ac, pvStr, current, kMin) {
   var lastChar = current.charAt(current.length - 1);
   if (
     pvStr.length < kMin ||
-    lastChar === ":" ||
-    lastChar === " " ||
-    current === ""
+    lastChar === ':' ||
+    lastChar === ' ' ||
+    current === ''
   ) {
-    pvStr = "";
-    ac.innerHTML = "";
-    ac.className = "";
+    pvStr = '';
+    ac.innerHTML = '';
+    ac.className = '';
   }
   return pvStr;
 };
@@ -133,18 +133,18 @@ var handleMatch = function (triggerMatch, currentText, cursorPos, keyCharMin) {
   var strStart = currentText.lastIndexOf(currentMatch);
   var strEnd = strStart + currentMatch.length;
 
-  log("cursorPos", cursorPos);
+  log('cursorPos', cursorPos);
   if (cursorPos >= strStart && cursorPos <= strEnd) {
     // twitch and other emoji
     if (
       currentMatch &&
       currentMatch.length >= keyCharMin &&
-      emoteChar === ":"
+      emoteChar === ':'
     ) {
       emojiUtils.addToPreviewList(emojiUtils.filterEmoji(currentMatch));
     }
   }
-  log("match", triggerMatch, strStart, strEnd);
+  log('match', triggerMatch, strStart, strEnd);
 
   return {
     start: strStart,
@@ -154,7 +154,7 @@ var handleMatch = function (triggerMatch, currentText, cursorPos, keyCharMin) {
 };
 
 var chatInputKeyupFunc = function (e) {
-  var acPreview = document.querySelector("#autocomplete-preview");
+  var acPreview = document.querySelector('#autocomplete-preview');
   var hasItems = acPreview.children.length > 0;
 
   if (e.keyCode === KEYS.enter && !hasItems) {
@@ -175,17 +175,17 @@ var chatInputKeyupFunc = function (e) {
 
   if ((e.keyCode === KEYS.enter || e.keyCode === KEYS.tab) && hasItems) {
     e.preventDefault();
-    previewList.selected = $(".preview-item.selected").find(".ac-text").text();
+    previewList.selected = $('.preview-item.selected').find('.ac-text').text();
     previewList.updateChatInput();
     return false;
   }
 
-  var currentText = this.value;
-  var cursorPos = $(this).get(0).selectionStart;
+  var currentText = e.target.value;
+  var cursorPos = e.target.selectionStart;
 
   var triggerMatch = currentText.match(inputRegex);
 
-  var previewSearchStr = "";
+  var previewSearchStr = '';
 
   if (triggerMatch && triggerMatch.length > 0) {
     var matchData = handleMatch(
@@ -198,46 +198,47 @@ var chatInputKeyupFunc = function (e) {
     previewList.data = matchData;
   }
 
-  log("inKeyup", previewList.data);
+  log('inKeyup', previewList.data);
 
   shouldClearPreview(acPreview, previewSearchStr, currentText, keyCharMin);
 };
 
 var chatInputKeydownFunc = function (e) {
-  var emptyPreview = !document.querySelector("#autocomplete-preview > li");
-  var isValidKey = _.includes(
-    [KEYS.tab, KEYS.enter, KEYS.up, KEYS.down, KEYS.left, KEYS.right],
+  const hasItems = document.querySelector('#autocomplete-preview > li');
+  const isValidKey = [KEYS.tab, KEYS.enter, KEYS.up, KEYS.down].includes(
     e.keyCode
   );
+  const isModifierKey = e.shiftKey || e.ctrlKey || e.altKey || e.metaKey;
 
-  // Manually send the keycodes if the preview popup isn't visible
-  if (emptyPreview || !isValidKey) {
-    QueUp.room.chat.ncKeyDown(e);
+  if (!isModifierKey && hasItems && isValidKey) {
+    e.preventDefault();
     return;
   }
 
-  // stop default behaviors of special keys so we can use them in preview
-  if (isValidKey && !emptyPreview) {
-    e.preventDefault();
-    e.stopPropagation();
-    e.stopImmediatePropagation();
+  // temporary fix to restore enter key functionality for sending messages
+  // due to the new multiline chat textarea
+  if (!isModifierKey && e.keyCode === KEYS.enter) {
+    window.QueUp.room.chat.sendMessage();
+    window.QueUp.room.chat.resizeTextarea();
+  } else if (!isModifierKey) {
+    window.QueUp.room.chat.ncKeyDown(e);
   }
 };
 
 myModule.turnOn = function () {
   previewList.init();
   QueUp.room.chat.delegateEvents(
-    _.omit(QueUp.room.chat.events, ["keydown #chat-txt-message"])
+    _.omit(QueUp.room.chat.events, ['keydown #chat-txt-message'])
   );
-  $(document.body).on("keydown", "#chat-txt-message", chatInputKeydownFunc);
-  $(document.body).on("keyup", "#chat-txt-message", chatInputKeyupFunc);
+  $('#chat-txt-message').on('keydown', chatInputKeydownFunc);
+  $('#chat-txt-message').on('keyup', chatInputKeyupFunc);
 };
 
 myModule.turnOff = function () {
   previewList.stop();
   QueUp.room.chat.delegateEvents(QueUp.room.chat.events);
-  $(document.body).off("keydown", "#chat-txt-message", chatInputKeydownFunc);
-  $(document.body).off("keyup", "#chat-txt-message", chatInputKeyupFunc);
+  $('#chat-txt-message').off('keydown', chatInputKeydownFunc);
+  $('#chat-txt-message').off('keyup', chatInputKeyupFunc);
 };
 
 module.exports = myModule;
