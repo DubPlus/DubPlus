88ac1c4f8fea2882bacca22e861082301c7015db
franciscog <557684+FranciscoG@users.noreply.github.com>
2024-07-04 11:30:37 -0400

    added missing jquery.js to extensions/common and updated core manifest to include it

--- extensions/common/scripts/loader.js
+++ extensions/common/scripts/loader.js
@@ -1,6 +1,1 @@
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.async = true;
-script.src = 'https://cdn.jsdelivr.net/gh/DubPlus/DubPlus/dubplus.js';
-
-document.getElementsByTagName('head')[0].appendChild(script);
+$.getScript('https://cdn.jsdelivr.net/gh/DubPlus/DubPlus/dubplus.js');
