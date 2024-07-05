215e9f62faa8e0a0d70fb931a6d359371c503386
franciscog <557684+FranciscoG@users.noreply.github.com>
2024-07-05 13:15:39 -0400

    updated error handling in tasks/index.js

--- tasks/index.js
+++ tasks/index.js
@@ -8,11 +8,11 @@ var sassTasks = require(process.cwd() + '/tasks/sassbundle.js');
 var extensionBuild = require(process.cwd() + '/tasks/extensions.js');
 
 // find out which task we're running
-var currentTask = process.argv[2]; 
+var currentTask = process.argv[2];
 var arg = process.argv[3];
 
-function onError(err){
-  log.error(err);
+function onError(src, err) {
+  log.error(src, err);
 }
 
 switch (currentTask) {
@@ -23,29 +23,49 @@ switch (currentTask) {
 
   // runs both bundling and minifying
   case 'bundle':
-    jsTasks.bundle()
+    jsTasks
+      .bundle()
       .then(jsTasks.minify)
-      .then(function(){ console.log('js bundling and minifying complete'); })
-      .catch(onError);
+      .then(function () {
+        console.log('js bundling and minifying complete');
+      })
+      .catch((err) => {
+        onError('js bundle', err);
+      });
     break;
 
   // separated out minifying just in case
   case 'min':
   case 'minify':
   case 'min-release':
-    jsTasks.minify()
-      .then(function(){ console.log('js minifying finished'); })
-      .catch(onError);
-    sassTasks.minify()
-      .then(function(){ console.log('sass finished minifying');})
-      .catch(onError);
+    jsTasks
+      .minify()
+      .then(function () {
+        console.log('js minifying finished');
+      })
+      .catch((err) => {
+        onError('js minify', err);
+      });
+    sassTasks
+      .minify()
+      .then(function () {
+        console.log('sass finished minifying');
+      })
+      .catch((err) => {
+        onError('sass minify', err);
+      });
     break;
 
   case 'sass':
-    sassTasks.compile()
+    sassTasks
+      .compile()
       .then(sassTasks.minify)
-      .then(function(){ console.log('sass finished compiling & minifying');})
-      .catch(onError);
+      .then(function () {
+        console.log('sass finished compiling & minifying');
+      })
+      .catch((err) => {
+        onError('sass compile and minify', err);
+      });
     break;
 
   case 'ext':
@@ -66,12 +86,26 @@ switch (currentTask) {
   case 'build':
   case 'build-release':
   default:
-    jsTasks.bundle()
+    jsTasks
+      .bundle()
       .then(jsTasks.minify)
-      .then(function(){ console.log('js finished bundling and minifying'); })
-      .catch(onError);
-    sassTasks.compile()
+      .then(function () {
+        console.log('js finished bundling and minifying');
+      })
+      .catch((err) => {
+        onError('js compile and minify', err);
+      });
+    sassTasks
+      .compile()
+      .catch((err) => {
+        console.log(err);
+        onError('sass compile', err);
+      })
       .then(sassTasks.minify)
-      .then(function(){ console.log('sass finished compiling & minifying');})
-      .catch(onError);
+      .then(function () {
+        console.log('sass finished compiling & minifying');
+      })
+      .catch((err) => {
+        onError('sass minify', err);
+      });
 }
