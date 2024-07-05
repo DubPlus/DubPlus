b411073a8453f56a2a4a075743bb1ea191c4c444
franciscog <557684+FranciscoG@users.noreply.github.com>
2024-07-05 13:18:17 -0400

    updating the deploy-ff script to use web-ext directly, work in progress

--- tasks/deploy-ff.js
+++ tasks/deploy-ff.js
@@ -2,20 +2,25 @@ const log = require('./colored-console.js');
 import webExt from 'web-ext';
 
 /****************************************************
- * Important note
- * web-ext needs to be installed GLOBALLY
- * npm i web-ext -g
+ * our ff addon link: https://addons.mozilla.org/en-US/firefox/addon/dubplus/
  *
  * more info on web-ext sign
  * https://developer.mozilla.org/en-US/Add-ons/WebExtensions/web-ext_command_reference#web-ext_sign
  *
- * this needs API keys i think
+ * This command:
+ *
+ * - creates a listing for your extension on AMO if --channel is set to listed and the extension isn't listed.
+ * - adds a version to a listed extension if the --channel is set to listed and your extension is listed.
+ * - downloads a signed copy of the extension if the --channel is set to unlisted.
  */
 
 function signFFext() {
   const options = {
-    cwd: process.cwd() + `/extensions/Firefox`,
-    encoding: 'utf8',
+    apiKey: process.env.WEB_EXT_API_KEY,
+    apiSecret: process.env.WEB_EXT_API_SECRET,
+    channel: 'listed',
+    timeout: 5 * 1000 * 60, // 5 minutes,
+    uploadSourceCode: process.cwd() + '/extensions/Firefox',
   };
   webExt.cmd.sign(options).then((result) => {
     log.info('Firefox extension signed');
