09b8e66b65013c0ecd32652f22910bad36413d70
franciscog <557684+FranciscoG@users.noreply.github.com>
2024-07-05 13:54:16 -0400

    removed jquery from extensions and added custom loader

--- extensions/common/scripts/loader.js
+++ extensions/common/scripts/loader.js
@@ -1,3 +1,10 @@
-$.getScript(
-  'https://cdn.jsdelivr.net/gh/DubPlus/DubPlus/dubplus.js?version=VERSION_REPLACED_BY_BUILD_SCRIPT'
-);
+const scriptTag = document.createElement('script');
+scriptTag.type = 'text/javascript';
+scriptTag.async = true;
+scriptTag.onerror = function (e) {
+  console.error('Dub+ failed to load', e);
+};
+scriptTag.src =
+  'https://cdn.jsdelivr.net/gh/DubPlus/DubPlus/dubplus.js?version=0.3.4';
+
+document.head.appendChild(scriptTag);
