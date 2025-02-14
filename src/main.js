2eaf80f1bf89c45197067e633f3ae2f9007af0d4
franciscog <557684+FranciscoG@users.noreply.github.com>
2025-02-14 13:19:02 -0500

    added todo in main js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import DubPlus from './DubPlus.svelte';
 import { loadCSS } from './utils/css';
 import { logError } from './utils/logger';
 
+// TODO: if this was loaded via the exension, we should not load this css
+// we should only be loading this if run as bookmarklet.
+// Have successfully loaded the css file from the extension but the font
+// is not loading.
 if (!import.meta.env.DEV) {
   loadCSS('/dubplus.css', 'dubplus-css').catch(logError);
 }
