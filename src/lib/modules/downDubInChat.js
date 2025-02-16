09f5e388197f6a0a225e2b35652955eb89f1fede
franciscog <557684+FranciscoG@users.noreply.github.com>
2025-02-16 02:03:03 -0500

    cleanup of down and updubs in chat

--- src/lib/modules/downDubInChat.js
+++ src/lib/modules/downDubInChat.js
@@ -2,24 +2,19 @@
  * Show downvotes in chat
  * only mods can use this
  */
-import { isMod } from "../../utils/modcheck";
-import { insertQueupChat } from "../../utils/chat-message";
-import { t } from "../stores/i18n.svelte";
+import { isMod } from '../../utils/modcheck';
+import { insertQueupChat } from '../../utils/chat-message';
+import { t } from '../stores/i18n.svelte';
 
 function downdubWatcher(e) {
-  // const user = window.QueUp.session.get("username");
-  // const currentDj = window.QueUp.room.users.collection.findWhere({
-  //   userid: window.QueUp.room.player.activeSong.attributes.song.userid,
-  // }).attributes._user.username;
-
   const isUserTheDJ =
     window.QueUp.session.id ===
     window.QueUp.room.player.activeSong.attributes.song.userid;
 
-  if (isUserTheDJ && e.dubtype === "downdub") {
+  if (isUserTheDJ && e.dubtype === 'downdub') {
     insertQueupChat(
-      "dubplus-chat-system-downdub",
-      t("downdubs-in-chat.chat-message", {
+      'dubplus-chat-system-downdub',
+      t('downdubs-in-chat.chat-message', {
         username: e.user.username,
         song_name: window.QueUp.room.player.activeSong.attributes.songInfo.name,
       })
@@ -28,18 +23,18 @@ function downdubWatcher(e) {
 }
 
 export const downdubsInChat = {
-  id: "downdubs-in-chat",
-  label: "downdubs-in-chat.label",
-  description: "downdubs-in-chat.description",
-  category: "general",
+  id: 'downdubs-in-chat',
+  label: 'downdubs-in-chat.label',
+  description: 'downdubs-in-chat.description',
+  category: 'general',
   modOnly: true,
   turnOn() {
     if (isMod(window.QueUp.session.id)) {
-      window.QueUp.Events.bind("realtime:room_playlist-dub", downdubWatcher);
+      window.QueUp.Events.bind('realtime:room_playlist-dub', downdubWatcher);
     }
   },
 
   turnOff() {
-    window.QueUp.Events.unbind("realtime:room_playlist-dub", downdubWatcher);
+    window.QueUp.Events.unbind('realtime:room_playlist-dub', downdubWatcher);
   },
 };
--- src/lib/modules/upDubInChat.js
+++ src/lib/modules/upDubInChat.js
@@ -3,23 +3,18 @@
  * only mods can use this
  */
 
-import { insertQueupChat } from "../../utils/chat-message";
-import { t } from "../stores/i18n.svelte";
+import { insertQueupChat } from '../../utils/chat-message';
+import { t } from '../stores/i18n.svelte';
 
 function updubWatcher(e) {
-  // const user = window.QueUp.session.get("username");
-  // const currentDj = window.QueUp.room.users.collection.findWhere({
-  //   userid: window.QueUp.room.player.activeSong.attributes.song.userid,
-  // }).attributes._user.username;
-
   const isUserTheDJ =
     window.QueUp.session.id ===
     window.QueUp.room.player.activeSong.attributes.song.userid;
 
-  if (isUserTheDJ && e.dubtype === "updub") {
+  if (isUserTheDJ && e.dubtype === 'updub') {
     insertQueupChat(
-      "dubplus-chat-system-updub",
-      t("updubs-in-chat.chat-message", {
+      'dubplus-chat-system-updub',
+      t('updubs-in-chat.chat-message', {
         username: e.user.username,
         song_name: window.QueUp.room.player.activeSong.attributes.songInfo.name,
       })
@@ -28,14 +23,14 @@ function updubWatcher(e) {
 }
 
 export const upDubInChat = {
-  id: "updubs-in-chat",
-  label: "updubs-in-chat.label",
-  description: "updubs-in-chat.description",
-  category: "general",
+  id: 'updubs-in-chat',
+  label: 'updubs-in-chat.label',
+  description: 'updubs-in-chat.description',
+  category: 'general',
   turnOn() {
-    window.QueUp.Events.bind("realtime:room_playlist-dub", updubWatcher);
+    window.QueUp.Events.bind('realtime:room_playlist-dub', updubWatcher);
   },
   turnOff() {
-    window.QueUp.Events.unbind("realtime:room_playlist-dub", updubWatcher);
+    window.QueUp.Events.unbind('realtime:room_playlist-dub', updubWatcher);
   },
 };
