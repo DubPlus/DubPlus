7c7f81c9df0027e472b07b778a300b907c41087f
franciscog <557684+FranciscoG@users.noreply.github.com>
2025-02-17 01:18:59 -0500

    removed a comment in main.js and emptied out container after unmount

--- src/main.js
+++ src/main.js
@@ -4,10 +4,6 @@ import DubPlus from './DubPlus.svelte';
 import { loadCSS } from './utils/css';
 import { logError } from './utils/logger';
 
-// TODO: if this was loaded via the exension, we should not load this css
-// we should only be loading this if run as bookmarklet.
-// Have successfully loaded the css file from the extension but the font
-// is not loading.
 if (!import.meta.env.DEV) {
   loadCSS('/dubplus.css', 'dubplus-css').catch((e) => {
     logError('Failed to load dubplus.css', e);
@@ -25,6 +21,7 @@ if (!container) {
 if (container.children.length > 0) {
   // Dub+ has already been loaded so we need to unmount it first
   unmount(container);
+  container.innerHTML = '';
 }
 
 const app = mount(DubPlus, {
