2ee77223d313dc698655981cc85837ce57e0c9f3
franciscog <557684+FranciscoG@users.noreply.github.com>
2025-02-21 01:29:49 -0500

    improved activeTabState

--- src/lib/stores/activeTabState.svelte.js
+++ src/lib/stores/activeTabState.svelte.js
@@ -1,9 +1,69 @@
+import { logInfo } from '../../utils/logger';
+
 export const activeTabState = $state({ isActive: true });
 
-window.onfocus = function () {
-  activeTabState.isActive = true;
-};
+/**
+ * An array of functions to call when the visibility state changes to hidden.
+ * @type {Array<() => void>}
+ */
+const onOut = [];
 
-window.onblur = function () {
-  activeTabState.isActive = false;
-};
+/**
+ * An array of functions to call when the visibility state changes to visible.
+ * @type {Array<() => void>}
+ */
+const onIn = [];
+
+document.addEventListener('visibilitychange', handleChange);
+
+window.onpageshow = handleChange;
+window.onpagehide = handleChange;
+window.onfocus = handleChange;
+window.onblur = handleChange; // Changing tab with alt+tab
+
+// Initialize state if Page Visibility API is supported
+if (document.hidden !== undefined) {
+  handleChange({ type: document.hidden ? 'blur' : 'focus' });
+}
+
+/**
+ *
+ * @param {Partial<PageTransitionEvent | FocusEvent>} evt
+ */
+function handleChange(evt) {
+  if (
+    activeTabState.isActive &&
+    (['blur', 'pagehide'].includes(evt.type) || document.hidden)
+  ) {
+    activeTabState.isActive = false;
+    onOut.forEach((fn) => fn());
+    logInfo('activeTabState', 'inactive');
+  } else if (
+    !activeTabState.isActive &&
+    (['focus', 'pageshow'].includes(evt.type) || !document.hidden)
+  ) {
+    activeTabState.isActive = true;
+    onIn.forEach((fn) => fn());
+    logInfo('activeTabState', 'active');
+  }
+}
+
+/**
+ *
+ * @param {() => void} inHandler
+ * @param {() => void} outHandler
+ */
+export function registerVisibilityChangeListeners(inHandler, outHandler) {
+  if (inHandler) onIn.push(inHandler);
+  if (outHandler) onOut.push(outHandler);
+}
+
+/**
+ *
+ * @param {() => void} inHandler
+ * @param {() => void} outHandler
+ */
+export function unRegisterVisibilityChangeListeners(inHandler, outHandler) {
+  if (inHandler) onIn.splice(onIn.indexOf(inHandler), 1);
+  if (outHandler) onOut.splice(onOut.indexOf(outHandler), 1);
+}
