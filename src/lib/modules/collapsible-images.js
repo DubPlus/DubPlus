87bbaa076d7e3563bc0346b7a8cf26ddaaa0084c
franciscog <557684+FranciscoG@users.noreply.github.com>
2025-08-29 00:54:00 -0400

    refactor collapsible-images module to improve code and guard against other issues

--- src/lib/modules/collapsible-images.js
+++ src/lib/modules/collapsible-images.js
@@ -1,26 +1,59 @@
+// import { CHAT_MESSAGE } from '../../events-constants';
 import { CHAT_MESSAGE } from '../../events-constants';
-import { getImagesInChat } from '../queup.ui';
+import { logInfo } from '../../utils/logger';
+import { waitFor } from '../../utils/waitFor';
+import { getChatContainer, getImagesInChat } from '../queup.ui';
+
+const COLLAPSED = 'dubplus-collapsed';
+const COLLAPSIBLE = 'dubplus-collapsible-image';
+const COLLAPSER = 'dubplus-collapser';
+const IMAGE_SELECTOR = 'autolink-image';
 
 /**
  *
- * @param {Event & { target: HTMLButtonElement }} event
+ * @param {HTMLButtonElement} expandButton
  */
-function handleCollapseButtonClick(event) {
-  const parentElement = event.target.parentElement;
-  const imageContaner = /**@type {HTMLAnchorElement}*/ (
-    event.target.previousElementSibling
+function handleCollapseButtonClick(expandButton) {
+  /**
+   * @type {HTMLElement} this will be the <p class="dubplus-collapsible-image">
+   * element that the button is inside of
+   */
+  const parentElement = expandButton.parentElement;
+
+  // the <a class="autolink-image"> element inside the parent.
+  // there should only be 1 autolink-image per parent. There is the possibility
+  // that there are img tags for emojis within the same parent.
+  const imageContainer = /**@type {HTMLAnchorElement}*/ (
+    parentElement.querySelector(`.${IMAGE_SELECTOR}`)
   );
 
-  if (!parentElement.classList.contains('dubplus-collapsed')) {
-    parentElement.classList.add('dubplus-collapsed');
-    event.target.title = 'expand image';
-    imageContaner.setAttribute('aria-hidden', 'true');
-    event.target.setAttribute('aria-expanded', 'false');
+  if (imageContainer) {
+    if (!parentElement.classList.contains(COLLAPSED)) {
+      parentElement.classList.add(COLLAPSED);
+      expandButton.title = 'expand image';
+      imageContainer.setAttribute('aria-hidden', 'true');
+      expandButton.setAttribute('aria-expanded', 'false');
+    } else {
+      parentElement.classList.remove(COLLAPSED);
+      expandButton.title = 'collapse image';
+      imageContainer.setAttribute('aria-hidden', 'false');
+      expandButton.setAttribute('aria-expanded', 'true');
+    }
   } else {
-    parentElement.classList.remove('dubplus-collapsed');
-    event.target.title = 'collapse image';
-    imageContaner.setAttribute('aria-hidden', 'false');
-    event.target.setAttribute('aria-expanded', 'true');
+    logInfo('No image container found in:', parentElement);
+  }
+}
+
+/**
+ * This is the handler should be attached the chat container.
+ * @param {Event} event
+ */
+function eventDelegatorHandler(event) {
+  if (
+    event.target instanceof HTMLButtonElement &&
+    event.target.classList.contains(COLLAPSER)
+  ) {
+    handleCollapseButtonClick(event.target);
   }
 }
 
@@ -29,46 +62,70 @@ function handleCollapseButtonClick(event) {
  */
 function addCollapserToImage(autolinkImage) {
   if (!autolinkImage) return;
-  if (
-    !autolinkImage.parentElement.classList.contains('dubplus-collapsible-image')
-  ) {
-    autolinkImage.parentElement.classList.add('dubplus-collapsible-image');
+  if (!autolinkImage.parentElement.classList.contains(COLLAPSIBLE)) {
+    autolinkImage.parentElement.classList.add(COLLAPSIBLE);
 
     const button = document.createElement('button');
     button.type = 'button';
     button.title = 'collapse image';
-    button.classList.add('dubplus-collapser');
-    button.addEventListener('click', handleCollapseButtonClick);
+    button.setAttribute('aria-expanded', 'true');
+    button.classList.add(COLLAPSER);
+    // button.addEventListener('click', handleCollapseButtonClick);
     autolinkImage.parentElement.appendChild(button);
   }
 }
 
 /**
  *
- * @param {import('../../events').ChatMessageEvent} [e]
+ * @param {import('../../events').ChatMessageEvent} e
  */
-function processChat(e) {
+function processNewChatMessage(e) {
   if (e?.chatid) {
-    const chatMessage = document.querySelector(`.chat-id-${e.chatid}`);
-    if (chatMessage) {
-      addCollapserToImage(chatMessage.querySelector('.autolink-image'));
-      return;
-    }
+    const chatSelector = `.chat-id-${e.chatid}`;
+
+    // sometimes the chat message isn't fully in the DOM yet when this event
+    // fires, so we wait for it to appear
+    waitFor(() => {
+      return !!document.querySelector(chatSelector);
+    }).then(() => {
+      const chatMessage = document.querySelector(chatSelector);
+      if (chatMessage) {
+        const unprocessedImages = findUnProcessedImages(chatMessage);
+        unprocessedImages.forEach(addCollapserToImage);
+      }
+    });
   }
+}
+
+function processAllChatMessages() {
   const chatImages = getImagesInChat();
   chatImages.forEach(addCollapserToImage);
 }
 
 function reset() {
-  document.querySelectorAll('.dubplus-collapsible-image').forEach((el) => {
-    el.classList.remove('dubplus-collapsible-image');
-    el.classList.remove('dubplus-collapsed');
+  // remove all classes added
+  document.querySelectorAll(`.${COLLAPSIBLE}`).forEach((el) => {
+    el.classList.remove(COLLAPSIBLE, COLLAPSED);
   });
-  document.querySelectorAll('.dubplus-collapser').forEach((el) => {
-    el.removeEventListener('click', handleCollapseButtonClick);
+  // remove all buttons from the UI. They will be added back when user
+  // turns this feature back on
+  document.querySelectorAll(`.${COLLAPSER}`).forEach((el) => {
     el.remove();
   });
+  // remove all attributes added
   getImagesInChat().forEach((el) => el.removeAttribute('aria-hidden'));
+}
+
+/**
+ *
+ * @param {Element} container
+ * @returns {Element[]}
+ */
+function findUnProcessedImages(container) {
+  const images = container.querySelectorAll(`.${IMAGE_SELECTOR}`);
+  return Array.from(images).filter(
+    (el) => !el.parentElement.classList.contains(COLLAPSIBLE),
+  );
 }
 
 /**
@@ -79,24 +136,24 @@ export const collapsibleImages = {
   label: 'collapsible-images.label',
   description: 'collapsible-images.description',
   category: 'general',
-  turnOn(onLoad) {
-    window.QueUp.Events.bind(CHAT_MESSAGE, processChat);
+  turnOn() {
+    window.QueUp.Events.bind(CHAT_MESSAGE, processNewChatMessage);
 
-    if (onLoad) {
-      // This is coming from a page load where Dub+ auto-enables this module
-      // based on the saved user settings. Sometimes the chat message aren't
-      // fully loaded yet so we need to wait a bit before processing the chat
-      setTimeout(() => {
-        processChat();
-      }, 1000);
-    } else {
-      // when user turns it on while already on the page, we don't need to wait
-      // to process the chat messages
-      processChat();
-    }
+    waitFor(() => {
+      return Boolean(getImagesInChat().length);
+    }).then(() => {
+      processAllChatMessages();
+    });
+
+    waitFor(() => {
+      return Boolean(getChatContainer());
+    }).then(() => {
+      getChatContainer()?.addEventListener('click', eventDelegatorHandler);
+    });
   },
   turnOff() {
-    window.QueUp.Events.unbind(CHAT_MESSAGE, processChat);
+    window.QueUp.Events.unbind(CHAT_MESSAGE, processNewChatMessage);
+    getChatContainer()?.removeEventListener('click', eventDelegatorHandler);
     reset();
   },
 };
